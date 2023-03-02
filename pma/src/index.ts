25fd598063b747435b3c90e541f27682d5007e7a
Rasmiyya-Mammadli <rashababayeva@gmail.com>
2023-03-02 14:23:29 +0400

    git fix

--- pma/src/index.ts
+++ pma/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://rasmiyyamammad:Roma123456789@cluster0.m6cshcu.mongodb.net/managerApp');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
